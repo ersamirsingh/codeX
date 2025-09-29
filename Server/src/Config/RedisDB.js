57044272d399af3cae14bda2e7d90b17edf33220
Samir Kumar Singh <ersamirsingh@gmail.com>
2025-09-29 11:14:54 +0530

    Update RedisDB.js

--- Server/src/Config/RedisDB.js
+++ Server/src/Config/RedisDB.js
@@ -2,7 +2,7 @@ const redis =  require('redis');
 
 const redisClient = redis.createClient({
     username: 'default',
-    password: 'q3IDkXKmkvVVlUCt1AvKbzMNJEcvjagh',
+    password: process.env.REDIS_KEY,
     socket: {
         host: 'redis-18443.c8.us-east-1-4.ec2.redns.redis-cloud.com',
         port: 18443,
