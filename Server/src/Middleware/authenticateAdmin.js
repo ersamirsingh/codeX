e9228f9f2526ee3533363302f76e360d3762e99c
Samir Kumar Singh <ersamirsingh@gmail.com>
2025-10-07 11:21:09 +0530

    Update authenticateAdmin.js

--- Server/src/Middleware/authenticateAdmin.js
+++ Server/src/Middleware/authenticateAdmin.js
@@ -2,13 +2,15 @@ const jwt = require('jsonwebtoken')
 const User = require('../Models/user')
 const redisClient = require('../Config/RedisDB')
 
+
+
 const authenticateAdmin = async (req, res, next)=>{
 
     try {
         
-        const {Token} = req.cookies
-        if(!Token)
-            throw new Error('Token not Found')
+        const Token = req.cookies?.Token || req.headers.authorization?.split(" ")[1]
+        if (!Token)
+            return res.status(401).send("Unauthorized")
 
         const isBlocked = await redisClient.exists(`Token: ${Token}`)
 
@@ -31,7 +33,7 @@ const authenticateAdmin = async (req, res, next)=>{
         next()
 
     } catch (error) {
-        return res.status(401).send('Error occured: '+error.message)
+        return res.status(401).send(error.message)
     }
 }
 
